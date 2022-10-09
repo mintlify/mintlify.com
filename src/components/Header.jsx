ca8987c38114680ff6f76c80fbed7d84c3d86b3c
Han Wang <hyw2@cornell.edu>
2022-10-09 11:47:34 -0700

    Update margins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -435,7 +435,7 @@ export function Header() {
                   <Link href="/start">
                     <div className="group flex items-center rounded-full bg-zinc-800/90 px-4 py-2 text-xs font-medium text-zinc-50 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition hover:bg-zinc-600 dark:bg-zinc-100 dark:text-zinc-800 dark:ring-white/10 dark:hover:bg-zinc-200 sm:text-sm">
                       Get Started
-                      <ChevronRightIcon className="ml-2 h-auto w-4 stroke-zinc-400 dark:stroke-zinc-600" />
+                      <ChevronRightIcon className="ml-1 h-auto w-4 stroke-zinc-400 dark:stroke-zinc-600" />
                     </div>
                   </Link>
                 </div>
