9ca6223cb180092d809f13440f2ad0a25016880a
Han Wang <44352119+hanywang2@users.noreply.github.com>
2022-12-18 21:56:58 -0800

    No wrap on CTA button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -433,7 +433,7 @@ export function Header() {
               <div className="flex items-center justify-end space-x-6 lg:flex-1">
                 <div className="pointer-events-auto">
                   <Link href="/start">
-                    <div className="group flex items-center rounded-full bg-zinc-800/90 px-4 py-2 text-xs font-medium text-zinc-50 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition hover:bg-zinc-600 dark:bg-zinc-100 dark:text-zinc-800 dark:ring-white/10 dark:hover:bg-zinc-200 sm:text-sm">
+                    <div className="group flex items-center rounded-full bg-zinc-800/90 px-4 py-2 text-xs font-medium text-zinc-50 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur whitespace-nowrap transition hover:bg-zinc-600 dark:bg-zinc-100 dark:text-zinc-800 dark:ring-white/10 dark:hover:bg-zinc-200 sm:text-sm">
                       Get Started
                       <ChevronRightIcon className="ml-1 h-auto w-4 stroke-zinc-400 dark:stroke-zinc-600" />
                     </div>
