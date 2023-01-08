8e13fe481f2e54e92daa691bceaa489b77ecd61f
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-01-07 17:10:21 -0800

    Updtate analytics setup

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -46,18 +46,17 @@ export default function Document() {
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
+            // GA
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
   
             gtag('config', 'G-NZ6RFCB9LN');
-          `}
-        </Script>
-        <Script>
-          {`
+
+            // Segment
             !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="AxmMlFE7EFIobCdh1eFxP3ja2bRRelMi";;analytics.SNIPPET_VERSION="4.15.3";
-            analytics.load("AxmMlFE7EFIobCdh1eFxP3ja2bRRelMi");
-            analytics.page();
+              analytics.load("AxmMlFE7EFIobCdh1eFxP3ja2bRRelMi");
+              analytics.page();
             }}();
           `}
         </Script>
