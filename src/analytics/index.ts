676bdab8706e6174d806f63a0c4ac79763e74009
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-01-08 01:03:18 -0800

    Lowercase start onboarding event tracking

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -5,5 +5,5 @@ export const SEGMENT_WRITE_KEY = 'AxmMlFE7EFIobCdh1eFxP3ja2bRRelMi';
 export const analytics = AnalyticsBrowser.load({ writeKey: SEGMENT_WRITE_KEY })
 
 export const trackStartOnboarding = (source: string) => {
-  analytics.track('Start Onboarding', { source })
+  analytics.track('start_onboarding', { source })
 }
