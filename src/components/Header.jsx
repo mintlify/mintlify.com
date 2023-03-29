f73e0818e1cc1f9cdb1a5379c0399ce864125c4b
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-03-29 16:49:24 -0700

    Launch start page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import Image from 'next/future/image'
-import { PopupButton } from '@typeform/embed-react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { Popover, Transition } from '@headlessui/react'
@@ -443,15 +442,14 @@ export function Header() {
                 <a href="https://dashboard.mintlify.com" className="pointer-events-auto text-zinc-800/90 dark:text-zinc-200 dark:text-zinc transition hover:text-primary dark:hover:text-light text-sm font-medium hidden lg:block mr-7">Login</a>
                 <div className="space-x-6 flex items-center justify-end">
                   <div className="pointer-events-auto">
-                    <PopupButton id="QuGaC3n6">
-                      <div
+                      <a
                         className="group flex items-center whitespace-nowrap rounded-full bg-zinc-800/90 px-4 py-2 text-xs font-medium text-zinc-50 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition hover:bg-zinc-600 dark:bg-zinc-100 dark:text-zinc-800 dark:ring-white/10 dark:hover:bg-zinc-200 sm:text-sm"
                         onClick={() => trackStartOnboarding('Header')}
+                        href="https://dashboard.mintlify.com/signup"
                       >
                         Get Started
                         <ChevronRightIcon className="ml-1 h-auto w-4 stroke-zinc-400 dark:stroke-zinc-600" />
-                      </div>
-                    </PopupButton>
+                      </a>
                   </div>
                   <div className="pointer-events-auto h-[24px]">
                     <ModeToggle />
