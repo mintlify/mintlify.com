b8eb2dee50e2863057b34f56606794ec9b7297cc
coffeewithone <coffeeandone@gmail.com>
2023-05-18 14:28:10 -0700

    added blog section and blog post, updated footer.

---
+++ src/pages/blogs/index.jsx
@@ -0,0 +1,67 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { SimpleLayout } from '@/components/SimpleLayout'
+import { formatDate } from '@/lib/formatDate'
+import { getAllArticles } from '@/lib/getAllArticles'
+
+function Article({ article }) {
+  return (
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/blogs/${article.slug}`}>{article.title}</Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read more</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+    </article>
+  )
+}
+
+export default function ArticlesIndex({ articles }) {
+  return (
+    <>
+      <Head>
+        <title>Blogs - Beautiful documentation that converts users</title>
+        <meta
+          name="Blogs"
+          content="Build the documentation you've always wanted. Beautiful out of the box, easy to maintain, and optimized for user engagement."
+        />
+      </Head>
+      <SimpleLayout
+        title="Blogs"
+        intro="Mintlify Chronicles: Navigating the World of Technical Documentation"
+      >
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
+    },
+  }
+}
