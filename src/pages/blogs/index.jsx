d1cf747ed0e9e0b3833d974ae548239c8cf2a5ca
coffeewithone <coffeeandone@gmail.com>
2023-05-22 15:16:23 -0700

    updated blog card

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -10,43 +10,41 @@ function Article({ article }) {
   return (
     <Card>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mb-16 cursor-pointer overflow-hidden rounded-lg transition-colors duration-200">
-          <article className="mb-16 cursor-pointer overflow-hidden rounded-lg transition-colors duration-200">
-            <a href={`/blogs/${article.slug}`}>
-              <div className="relative flex w-full justify-center">
-                <Image
-                  src={article.imageUrl.src}
-                  width={700}
-                  height={300}
-                  objectFit="cover"
-                  className="rounded-2xl"
-                  alt="blog thumbnail"
-                />
-              </div>
-            </a>
-
-            <div className="max-w-2xl">
-              <div className="mt-8 flex items-center gap-x-4 text-xs">
-                <time dateTime={article.date} className="text-gray-500">
-                  {formatDate(article.date)}
-                </time>
-              </div>
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6  ">
-                  <a href={`/blogs/${article.slug}`}>
-                    <Card.Title>
-                      <span className="absolute inset-0"></span>
-                      {article.title}
-                    </Card.Title>
-                  </a>
-                </h3>
-                <Card.Description className="line-clamp-3 mt-5 text-sm leading-6 ">
-                  {article.description}
-                </Card.Description>
-              </div>
+        <article className="mb-16 max-w-lg cursor-pointer overflow-hidden rounded-lg transition-colors duration-200">
+          <a href={`/blogs/${article.slug}`}>
+            <div className="relative flex w-full justify-center">
+              <Image
+                src={article.imageUrl.src}
+                width={700}
+                height={300}
+                objectFit="cover"
+                className="rounded-2xl"
+                alt="blog thumbnail"
+              />
             </div>
-          </article>
-        </div>
+          </a>
+
+          <div className="max-w-2xl">
+            <div className="mt-8 flex items-center gap-x-4 text-xs">
+              <time dateTime={article.date} className="text-gray-500">
+                {formatDate(article.date)}
+              </time>
+            </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6  ">
+                <a href={`/blogs/${article.slug}`}>
+                  <Card.Title>
+                    <span className="absolute inset-0"></span>
+                    {article.title}
+                  </Card.Title>
+                </a>
+              </h3>
+              <Card.Description className="line-clamp-3 mt-5 text-sm leading-6 ">
+                {article.description}
+              </Card.Description>
+            </div>
+          </div>
+        </article>
       </div>
     </Card>
   )
