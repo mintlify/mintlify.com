88a8c749654b729eab6d340081e9ed9e3176fbaf
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-07-09 21:26:07 -0700

    Fix blog back button

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router'
 import { Container } from '@/components/Container'
 import { Prose } from '@/components/Prose'
 import { Author } from '@/pages/blog'
+import Link from 'next/link'
 
 function ArrowLeftIcon(props) {
   return (
@@ -31,17 +32,15 @@ export function ArticleLayout({
       <Container className="mt-13 lg:mt-24">
         <div className="xl:relative">
           <div className="mx-auto max-w-3xl">
-            <button
-              type="button"
-              onClick={() => router.back()}
-              aria-label="Go back to articles"
+            <Link
               className="mt-12 group mb-8 flex items-center justify-center transition lg:absolute lg:-left-5 lg:mb-0 xl:-top-0.5 xl:left-0 xl:mt-0 text-zinc-600 hover:text-zinc-900 dark:text-zinc-500 dark:hover:text-zinc-300 text-sm space-x-0.5 font-medium"
+              href="/blog"
             >
               <ArrowLeftIcon className="h-6 w-6" />
               <span>
                 Back
               </span>
-            </button>
+            </Link>
             <article>
               <header className="flex flex-col">
                 <h1 className="mt-4 text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
