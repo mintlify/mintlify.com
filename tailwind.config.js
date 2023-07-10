cd33065c750f87aa41bd52f6674d750de08a9996
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-07-09 17:47:58 -0700

    Add start to YC story

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,jsx,mdx}'],
   darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
   theme: {
