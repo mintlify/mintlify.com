ebdcc1164fd5bf056b0159b1fe7f2ad9942a9429
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-07-19 16:48:39 -0700

    Update features on landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ const features = [
     ),
     title: 'Built for performance',
     description:
-      'The platform is meticulously designed and implemented to bring users to where they need to be. Everything is also optimized for a great user experience, from performance to navigation.',
+      'Everything is meticulously designed and optimized for a great user experience, from performance to navigation.',
     cta: {
       name: 'View showcase',
       href: '/showcase',
@@ -278,6 +278,25 @@ const features = [
     cta: {
       name: 'Get started',
       href: '/start',
+    },
+  },
+  {
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path fillRule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z" clipRule="evenodd" />
+      </svg>
+    ),
+    title: 'AI enabled',
+    description:
+      `Access the limitless power of AI, right inside your documentation.`,
+    cta: {
+      name: 'Learn more',
+      href: '/blog/chat',
     },
   },
 ]
