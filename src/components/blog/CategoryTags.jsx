412052d21c6a474f9822ac9063e0f3c6fa058422
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-08-29 20:44:18 -0700

    Add dark mode for the tags

--- src/components/blog/CategoryTags.jsx
+++ src/components/blog/CategoryTags.jsx
@@ -5,8 +5,8 @@ function Tag({ title, onClick, isSelected }) {
   return (
     <div
       className={clsx(
-        'cursor-pointer rounded-md border px-3 py-px text-xs hover:bg-zinc-50',
-        isSelected ? 'border-green-700 text-green-800' : 'border-zinc-200'
+        'cursor-pointer rounded-md border px-3 py-px text-xs hover:bg-zinc-50 dark:text-zinc-100 dark:hover:bg-transparent',
+        isSelected ? 'border-green-700 text-green-800 dark:border-green-400/50 dark:text-green-300/90' : 'border-zinc-200 dark:border-zinc-700 dark:hover:border-zinc-500'
       )}
       onClick={onClick}
     >
