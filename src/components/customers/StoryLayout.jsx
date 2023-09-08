987bd589d0b7e56c3e9caba11501713fe7e15089
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-09-08 02:37:17 -0700

    Fix mobile responsiveness for story

--- src/components/customers/StoryLayout.jsx
+++ src/components/customers/StoryLayout.jsx
@@ -23,17 +23,17 @@ export function StoryLayout({ children, meta, about }) {
         <meta name="description" content={meta.description} />
         <meta property="og:image" content={meta.desktopImageUrl} />
       </Head>
-      <Container className="mt-13 lg:mt-24">
-        <div className="xl:relative">
+      <Container className="mt-16 lg:mt-24">
+        <div className="relative">
           <div className="grid-cols mx-auto grid grid-cols-8 items-start">
             <Link
-              className="b-8 group col-span-1 mt-12 flex items-center justify-start space-x-0.5 text-left text-sm font-medium text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-500 dark:hover:text-zinc-300 lg:-left-5 lg:mb-0 xl:-top-0.5 xl:left-0 xl:mt-0"
+              className="b-8 group col-span-1 mt-12 hidden lg:flex items-center justify-start space-x-0.5 text-left text-sm font-medium text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-500 dark:hover:text-zinc-300 lg:-left-5 lg:mb-0 xl:-top-0.5 xl:left-0 xl:mt-0"
               href="/customers"
             >
               <ArrowLeftIcon className="h-5 w-5" />
               <span>Back</span>
             </Link>
-            <article className="col-span-6">
+            <article className="col-span-8 lg:col-span-6">
               <header className="flex flex-col gap-6">
                 <span className="text-primary dark:text-light font-medium text-sm">Customer story</span>
                 <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100">
@@ -44,7 +44,7 @@ export function StoryLayout({ children, meta, about }) {
                 </h2>
               </header>
               <div className="grid-cols mt-6 grid grid-cols-6 gap-12">
-                <div className="sticky top-0 col-span-2 h-screen pr-4 pt-12">
+                <div className="lg:sticky top-0 col-span-6 lg:col-span-2 max-h-screen pr-4 pt-12">
                   <img
                     src={about.imageUrl}
                     className="block w-12 rounded-md object-cover"
@@ -61,7 +61,7 @@ export function StoryLayout({ children, meta, about }) {
                     Get Started
                   </Button>
                 </div>
-                <Prose className="col-span-4 mt-4">{children}</Prose>
+                <Prose className="col-span-6 lg:col-span-4 mt-4">{children}</Prose>
               </div>
             </article>
           </div>
