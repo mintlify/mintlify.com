e1b44af242c64451c675721012eae86812309fba
Han Wang <44352119+hanywang2@users.noreply.github.com>
2023-11-16 16:46:06 -0800

    Simplify all commas

--- src/pages/thank-you.jsx
+++ src/pages/thank-you.jsx
@@ -6,7 +6,7 @@ export default function ThankYou() {
   return (
     <>
       <Head>
-        <title>You’re subscribed - Mintlify</title>
+        <title>You're subscribed - Mintlify</title>
         <meta
           name="description"
           content="Thanks for subscribing to my newsletter."
@@ -14,7 +14,7 @@ export default function ThankYou() {
       </Head>
       <SimpleLayout
         title="Thanks for subscribing."
-        intro="I’ll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you’d want to hear about. You can unsubscribe at any time, no hard feelings."
+        intro="I'll send you an email any time I publish a new blog post, release a new project, or have anything interesting to share that I think you'd want to hear about. You can unsubscribe at any time, no hard feelings."
       />
     </>
   )
